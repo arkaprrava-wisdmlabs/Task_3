6c188aca805e33c13dffbb04154bbfbb04c6b70d
arkaprrava-wisdmlabs <arkaprava.chakraborty@wisdmlabs.com>
2023-06-19 10:36:24 +0530

    added shortcode for showing course categories

--- public/class-wdm-course-category-featured-image-public.php
+++ public/class-wdm-course-category-featured-image-public.php
@@ -1,9 +1,96 @@
 <?php
 if( ! class_exists( 'WDM_Plugin_Public' )){
     class WDM_Course_Category_Featured_Image_Public{
-        protected $plugin_name;
-        public function __construct($plugin_name){
-            $this->plugin_name = $plugin_name;
+        /**
+         * plugin direcory url
+         *
+         * @var [type]
+         */
+        protected $plugin_dir_url;
+        /**
+         * defines the plugin directory url
+         *
+         * @param [type] $plugin_name
+         */
+        public function __construct($plugin_dir_url){
+            $this->plugin_dir_url = $plugin_dir_url;
+        }
+        /**
+         * registerd and enqueued style for the shortcode in the frontend
+         *
+         * @return void
+         */
+        public function wdm_enqueue_styles(){
+            wp_register_style( 'shortcode_css', $this->plugin_dir_url.'public/assets/css/shortcode-css.css');
+            wp_enqueue_style( 'shortcode_css' );
+        }
+        /**
+         * add a shortcode to show th course catgories in the frontend
+         *
+         * @param [type] $atts
+         * @return void
+         */
+        public function wdm_shortcode($atts){
+            $out = '<div class="course-category">';
+            $head = '<center class="course_category_heading"><h2>Course Categories</h2></center>';
+            $number_of_course_category = 0;
+            if(isset($atts['categorynumber'])){
+                $number_of_course_category = $atts['categorynumber'];
+            }
+            $page_link = get_permalink();
+            $content_style = '';
+            $card_style = '';
+            $category_link = '';
+            $category_page = get_page_by_title('Course Categories');
+            if(!empty($category_page)){
+                if( $category_page->post_status === 'publish' ){
+                    $category_link = get_permalink( $category_page );
+                }
+            }
+            if($page_link === $category_link){
+                $content_style = 'style="margin-left:-40px"';
+                $card_style = 'style="width:360px;"';
+            }
+            else{
+                $out .= $head;
+            }
+            if( empty( $category_link ) ){
+                $category_link = $page_link;
+            }
+            $out .= '<div class="course_category_content"'.$content_style.' >';
+            $terms = get_terms(array( 'taxonomy' => 'ld_course_category', 'number' => $number_of_course_category ));
+            if( empty($terms) ){
+                $out .= '<p>'.__('No Course Category found', 'cfi') .'</p></div>';
+                return $out;
+            }
+            $a = 0;
+            foreach($terms as $term){
+                if($a%3 === 0){
+                    $out .= '<div class="row">';
+                }
+                $out .= '<a href="'.get_term_link($term, 'ld_course_category').'"><div class="course_category_card"'.$card_style.' >';
+                $out .= '<div class="figure"><img src="'.$this->plugin_dir_url.'public/assets/images/course-category-image.jpeg" alt="Course Category image"></div>';
+                $out .= '<div class="course_category_card_content">';
+                $out .= '<h4>'.$term->name.'</h4>';
+                $count = $term->count;
+                $text = ' Courses';
+                if($count < 2){
+                    $text = ' Course';
+                }
+                $out .= '<p>'.$term->count.__($text, 'cfi').'</p>';
+                $out .= '</div>';
+                $out .= '</div></a>';
+                if($a%3 === 2){
+                    $out .= '</div>';
+                }
+                $a += 1;
+            }
+            $out .= '</div>';
+            if( $number_of_course_category > 0 ){
+                $out .= '<center class="course_category_link"><a href="'.$category_link.'"><button>'.__('See More', 'cfi').'</button></a></center>';
+            }
+            $out .= '</div>';
+            return $out;
         }
     }
 }
--- wdm-course-category-featured-image-plugin.php
+++ wdm-course-category-featured-image-plugin.php
@@ -15,8 +15,10 @@ defined( 'ABSPATH' ) || die;
 if( ! class_exists( 'WDM_Course_Category_Featured_Image_Plugin' ) ){
     class WDM_Course_Category_Featured_Image_Plugin{
         protected $plugin_name;
-        public function __construct($plugin_name){
+        protected $plugin_dir_url;
+        public function __construct($plugin_name, $plugin_dir_url){
             $this->plugin_name = $plugin_name;
+            $this->plugin_dir_url = $plugin_dir_url;
             $this->define_admin_hooks();
             $this->define_public_hooks();
         }
@@ -27,9 +29,10 @@ if( ! class_exists( 'WDM_Course_Category_Featured_Image_Plugin' ) ){
         }
         public function define_public_hooks(){
             require_once plugin_dir_path( __FILE__ ) . 'public/class-wdm-course-category-featured-image-public.php';
-            $public = new WDM_Course_Category_Featured_Image_Public($this->plugin_name);
-            // add_action( 'hook', array( $public, 'function' ), 10, 0);
+            $public = new WDM_Course_Category_Featured_Image_Public($this->plugin_dir_url);
+            add_action( 'wp_enqueue_scripts',array( $public, 'wdm_enqueue_styles' ) , 10, 0);
+            add_shortcode( 'shortcode',array( $public, 'wdm_shortcode' ) , 10, 1);
         }
     }
 }
-new WDM_Course_Category_Featured_Image_Plugin(plugin_basename( __FILE__ ));
+new WDM_Course_Category_Featured_Image_Plugin(plugin_basename( __FILE__ ), plugin_dir_url( __FILE__ ));
